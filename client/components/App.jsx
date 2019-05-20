f36dc8e2ed8328cfe528cc03b100635c7145476d
jesse collier <jesse.collier97@gmail.com>
2019-05-21 11:40:39 +1200

    SOMETHING IS RENDERING

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment as F } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <F>
+        <p>react is running</p>
+      </F>
+    );
+  }
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById('app'));
+});
