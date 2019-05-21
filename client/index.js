eb3843879a2728c70d31873ee3e6806ba8a3e847
jesse collier <jesse.collier97@gmail.com>
2019-05-21 12:25:04 +1200

    added redux functionality

--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+);
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById("app"));
 });
